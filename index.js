dbcb829e4444a51b62acfef97e28075d5b67e120
Gianni <gianniicherry@gmail.com>
2022-09-19 16:53:32 -0700

    commit tree dd5cec4c7d6bb0fc049da58f41820299b2a23713

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = (num1* num2);
 
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 32
+let num4 = 8
+
+const mod = num3/num4
+
+const max = Math.max(1,3,7,8,20);
 // Write your code here
